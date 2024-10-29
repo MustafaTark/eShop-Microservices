3741fae27545594e74ad4a2191973089128cb24d
mostafatarek <mostaf01230@gmail.com>
2024-10-29 17:22:43 +0300

    Add Discount grpc

---
+++ src/Services/Discount/Discount/Models/Counpon.cs
@@ -0,0 +1,10 @@
+﻿namespace Discount.Models
+{
+    public class Counpon
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public int Amount { get; set; }
+    }
+}
