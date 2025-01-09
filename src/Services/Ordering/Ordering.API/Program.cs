fe314fdc5773f318395bf35d6ef387b42483f797
mostaf01230 <mostaf01230@gmail.com>
2025-01-09 19:01:55 +0200

    Add Order Service

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,3 +1,16 @@
+using Ordering.Infrastructure.Data.Extentions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services
+//    .AddInfrastructureServices(builder.Configuration)
+
+
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    await app.InitialiseDatabaseAsync();
+}
 app.Run();
